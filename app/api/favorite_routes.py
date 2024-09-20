93f59742feed79b9c6121641e969feaa2d22ccb4
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-09-20 14:39:22 -0700

    favs are up and working

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -1,5 +1,54 @@
 from flask import Blueprint
-from flask_login import login_required
-from app.models import User
+from flask_login import login_required, current_user
+from app.models import User, Favorite, db, Coffee
+from sqlalchemy import and_
 
 favorite_routes = Blueprint('favorites', __name__)
+
+@favorite_routes.route('')
+@login_required
+def favs():
+    '''
+    Get all favorites based off of current user. returns a list of all favorites
+    '''
+    users_favs = Favorite.query.filter(Favorite.user_id == current_user.id).all()
+    # will want to return the coffee as well
+    return [coffee.to_dict_basic() for coffee in users_favs]
+
+
+@favorite_routes.route('/<int:coffee_id>', methods=['POST'])
+@login_required
+def new_fav(coffee_id):
+    '''
+    Create a new favorite for the current user. checks if coffee exists and if you already favorite it
+    '''
+    already_exist = Favorite.query.filter(Favorite.coffee_id == coffee_id and Favorite.user_id == current_user.id).first()
+    if already_exist:
+        return {'errors':'This favorite already exists!'}
+
+    this_coffee = Coffee.query.filter(Coffee.id == coffee_id)
+    if not this_coffee:
+        return{'errors':'This coffee doesnt exist! Please try again'}
+
+    new_favorite = Favorite(user_id = current_user.id, coffee_id=coffee_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return new_favorite.to_dict_basic()
+
+
+@favorite_routes.route('/delete/<int:fav_id>', methods=['DELETE'])
+@login_required
+def remove_fav(fav_id):
+    '''
+    Deletes favorite for current user if user owns favorite and if favorite exists
+    '''
+    curr_fav = Favorite.query.filter(Favorite.id == fav_id).first()
+
+    if not curr_fav:
+        return {'errors': 'current favorite cannot be found'}, 404
+    if not curr_fav.user_id == current_user.id:
+        return {'errors': 'This is not your favorite. How are you even doing this?'}, 403
+
+    db.session.delete(curr_fav)
+    db.session.commit()
+    return {'message': 'Successfully deleted!'}, 200
