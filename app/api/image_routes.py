6025aae1a8650d1bee87048527f628acc78cd9a7
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-09-20 16:21:44 -0700

    added get 1 get all and delete image

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, redirect, render_template
-from app.models import db, CoffeeImage
+from app.models import db, CoffeeImage, Coffee
 from flask_login import current_user, login_required
 from ..forms.img_form import ImageForm
 from app.api.aws_helpers import (
-    upload_file_to_s3, get_unique_filename)
+    upload_file_to_s3, get_unique_filename, remove_file_from_s3)
 
 image_routes = Blueprint("images", __name__)
 
@@ -44,3 +44,54 @@ def upload_image():
 
     # return render_template("post_form.html", form=form, errors=None)
     return {"good": "good"}
+
+
+@image_routes.route('')
+def get_all_images():
+    '''
+    get all coffee images
+    '''
+    all_coffee = CoffeeImage.query.all()
+    return [coffee.to_dict_basic() for coffee in all_coffee]
+
+
+@image_routes.route('/<int:coffee_id>')
+def get_imgs_for_one_coffee(coffee_id):
+    '''
+    gets all images for one coffee
+    '''
+    one_coffee = CoffeeImage.query.filter(CoffeeImage.coffee_id == coffee_id)
+
+    return [coffee.to_dict_basic() for coffee in one_coffee]
+
+
+@image_routes.route('/<int:img_id>', methods=['DELETE'])
+@login_required
+def delete_image(img_id):
+    '''
+    deletes the selected image if they are the owner of the coffee
+    '''
+    img = CoffeeImage.query.filter(CoffeeImage.id == img_id).first()
+    if not img:
+        return {"errors":"Image does not exist!"},404
+
+    coffee = img.coffee.to_dict_basic()
+    # print('\n\nthis is coffee', coffee)
+    # print('\n\nthis is coffee', type(coffee))
+    # print('\n\nthis is coffee', dir(coffee))
+    # print('\n\nthis is coffee', coffee['ownerId'])
+    # print('\n\nthis is coffees owner id', coffee.values())
+
+    if not coffee:
+        return {"errors":"Coffee does not exist!"},404
+
+    if coffee['ownerId'] == current_user.id:
+        remove_file_from_s3(img.url)
+        db.session.delete(img)
+        db.session.commit()
+        return {"messages":" Successfully deleted!"},200
+    else:
+        # not img.coffee.to_dict_basic().user_id == current_user.id
+        return {"message":"forbidden"},403
+
+    # return {"error":"howd you get here"}
