f770051f03c9129344ab39c74ef7f4c065a611e6
Yicheng Wang <yichengw1011@gmail.com>
2024-09-20 20:41:05 -0700

    fix merge in form init file

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,7 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
-<<<<<<< HEAD
 from .coffee_form import CoffeeForm
-=======
 from .reviews_form import ReviewsForm
->>>>>>> feature-review-routes
