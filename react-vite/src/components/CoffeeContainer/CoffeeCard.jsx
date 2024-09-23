3ab1791e5c8cf8b9cd433e7f9b4b2b9925427f52
chris <chris.hubbard.dev@gmail.com>
2024-09-23 16:21:21 -0700

    all data added to landing page and coffee cards

--- react-vite/src/components/CoffeeContainer/CoffeeCard.jsx
+++ react-vite/src/components/CoffeeContainer/CoffeeCard.jsx
@@ -1,14 +1,15 @@
 import {Link} from 'react-router-dom'
+import { FaRegStar } from "react-icons/fa";
 
 
 function CoffeeCard({coffee}) {
-  
+
   return (
     <Link to={`/${coffee.id}`}>
       <h2>{coffee.name}</h2>
       <p>${coffee.price}</p>
+      {<p>{coffee.reviews.length < 1 ? 'New' : (coffee.reviews.reduce((sum, coffee) => sum + coffee.stars, 0) / coffee.reviews.length).toFixed(1)}<FaRegStar /> </p>}
       {coffee.coffeeImages.length > 0 && <img style={{width: '30rem'}}src={coffee.coffeeImages[0].url} alt="coffee-image" />}
-      {<p>{coffee.reviews.length < 1 ? 'New' : (coffee.reviews.reduce((sum, coffee) => sum + coffee.stars, 0) / coffee.reviews.length).toFixed(1)}</p>}
     </Link>
   )
 }
