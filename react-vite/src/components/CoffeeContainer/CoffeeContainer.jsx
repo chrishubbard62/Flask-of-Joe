1ea1e71455246612db34964c79c4beeb1128a305
chris <chris.hubbard.dev@gmail.com>
2024-10-14 17:07:46 -0700

    got the filters working

--- react-vite/src/components/CoffeeContainer/CoffeeContainer.jsx
+++ react-vite/src/components/CoffeeContainer/CoffeeContainer.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getCoffeesThunk } from '../../redux/coffee'
 import CoffeeCard from './CoffeeCard'
 import banner from '/banner.jpg'
@@ -10,10 +10,23 @@ function CoffeeContainer() {
   const data = useSelector(state => state.coffee)
   const dispatch = useDispatch()
   const coffees = Object.values(data)
+  const [filtered, setFiltered] = useState(coffees)
 
   useEffect(() => {
     dispatch(getCoffeesThunk())
   }, [dispatch])
+
+  useEffect(() => {
+    setFiltered(Object.values(data))
+  }, [data])
+
+  const handleCategory = (type) => {
+    if(type === 'all') {
+      setFiltered(coffees)
+    } else {
+      setFiltered(coffees.filter(coffee => coffee.roast === type))
+    }
+  }
 
   if (!data || !coffees) return <h2>Loading</h2>
 
@@ -23,8 +36,15 @@ function CoffeeContainer() {
         <img src={banner} alt="coffee banner" className='banner-image' />
       </div>
       <div className='landing-outer-container'>
+      <div className='coffee-landing-filters'>
+        <button  onClick={() => handleCategory('all')} className='filter-button'>All</button>
+        <button  onClick={() => handleCategory('Light')} className='filter-button'>Light</button>
+        <button  onClick={() => handleCategory('Medium')} className='filter-button'>Medium</button>
+        <button  onClick={() => handleCategory('Dark')} className='filter-button'>Dark</button>
+        <button  onClick={() => handleCategory('Espresso')} className='filter-button'>Espresso</button>
+      </div>
         <div className='landing-coffee-container'>
-          {coffees?.map((coffee) => {
+          {filtered?.map((coffee) => {
             return <CoffeeCard key={coffee?.id} coffee={coffee} />
           })}
         </div>
