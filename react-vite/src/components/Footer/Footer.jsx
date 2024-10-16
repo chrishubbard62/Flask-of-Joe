10988fc1f254dfd30d1d086e9754abec0e74c8bc
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-10-16 12:51:49 -0700

    changed max width of footer

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -7,33 +7,35 @@ import '/chris.jpg';
 const Footer = () => {
     return (
         <div className="footer-footer">
-            <div className='personal-div-individual'>
-                <img className='personal-images' src='chris.jpg'/>
-                <div className='personal-div-inner-text'>
-                    Chris Hubbard
-                    <div className='icons-for-individual'>
-                        <a target='_blank' rel="noreferrer" href='https://github.com/chrishubbard62' ><FaGithub /></a>
-                        <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/chris-hubbard-93409324/' ><FaLinkedin /></a>
+            <div className='inner-footer-box'>
+                <div className='personal-div-individual'>
+                    <img className='personal-images' src='chris.jpg' />
+                    <div className='personal-div-inner-text'>
+                        Chris Hubbard
+                        <div className='icons-for-individual'>
+                            <a target='_blank' rel="noreferrer" href='https://github.com/chrishubbard62' ><FaGithub /></a>
+                            <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/chris-hubbard-93409324/' ><FaLinkedin /></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='personal-div-individual'>
-                <img className='personal-images' src='lalo.jpg' />
-                <div className='personal-div-inner-text'>
-                    Gerardo Bonilla Jr
-                    <div className='icons-for-individual'>
-                        <a target='_blank' rel="noreferrer" href='https://github.com/Lalo-B' ><FaGithub /></a>
-                        <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/gerardo-bonilla-jr-47453b1bb/' ><FaLinkedin /></a>
+                <div className='personal-div-individual'>
+                    <img className='personal-images' src='lalo.jpg' />
+                    <div className='personal-div-inner-text'>
+                        Gerardo Bonilla Jr
+                        <div className='icons-for-individual'>
+                            <a target='_blank' rel="noreferrer" href='https://github.com/Lalo-B' ><FaGithub /></a>
+                            <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/gerardo-bonilla-jr-47453b1bb/' ><FaLinkedin /></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='personal-div-individual'>
-                <img className='personal-images' src='yicheng.jpg' />
-                <div className='personal-div-inner-text'>
-                    Yicheng Wang
-                    <div className='icons-for-individual'>
-                        <a target='_blank' rel="noreferrer" href='https://github.com/lunaako' ><FaGithub /></a>
-                        <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/yicheng-wang-b43604261/' ><FaLinkedin /></a>
+                <div className='personal-div-individual'>
+                    <img className='personal-images' src='yicheng.jpg' />
+                    <div className='personal-div-inner-text'>
+                        Yicheng Wang
+                        <div className='icons-for-individual'>
+                            <a target='_blank' rel="noreferrer" href='https://github.com/lunaako' ><FaGithub /></a>
+                            <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/yicheng-wang-b43604261/' ><FaLinkedin /></a>
+                        </div>
                     </div>
                 </div>
             </div>
